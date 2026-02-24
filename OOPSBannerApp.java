03f149ceb769d92c2ece624c17e5ca2615f022ce
456-uday <udaysreemaniksharma@gmail.com>
2026-02-24 14:11:58 +0530

    UC6: Static helper methods implemented for O, P, S

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -4,18 +4,49 @@ public class OOPSBannerApp {
 
         String[] banner = {
 
-            String.join("   ", "OOOO", "OOOO", "PPPP", "SSSS"),
-            String.join("   ", "O  O", "O  O", "P  P", "S   "),
-            String.join("   ", "O  O", "O  O", "PPPP", "SSS "),
-            String.join("   ", "O  O", "O  O", "P   ", "   S"),
-            String.join("   ", "OOOO", "OOOO", "P   ", "SSSS"),
-            String.join("   ", "    ", "    ", "    ", "    "),
-            String.join("   ", "UC5 ", "ARRAY", "INIT ", "    ")
+            String.join("   ", printO(1), printO(1), printP(1), printS(1)),
+            String.join("   ", printO(2), printO(2), printP(2), printS(2)),
+            String.join("   ", printO(3), printO(3), printP(3), printS(3)),
+            String.join("   ", printO(4), printO(4), printP(4), printS(4)),
+            String.join("   ", printO(5), printO(5), printP(5), printS(5))
 
         };
 
         for (String line : banner) {
             System.out.println(line);
         }
+    }
+
+    static String printO(int line) {
+        switch(line) {
+            case 1:
+            case 5: return "OOOO";
+            case 2:
+            case 3:
+            case 4: return "O  O";
+        }
+        return "";
+    }
+
+    static String printP(int line) {
+        switch(line) {
+            case 1: return "PPPP";
+            case 2:
+            case 3: return "P  P";
+            case 4:
+            case 5: return "P   ";
+        }
+        return "";
+    }
+
+    static String printS(int line) {
+        switch(line) {
+            case 1: return "SSSS";
+            case 2: return "S   ";
+            case 3: return "SSS ";
+            case 4: return "   S";
+            case 5: return "SSSS";
+        }
+        return "";
     }
 }
